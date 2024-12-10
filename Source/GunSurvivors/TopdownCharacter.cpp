f368347a120b6348dabe3f76b0fdf796d9343b3d
berksarikayaa <berksarikaaya@gmail.com>
2024-12-10 03:31:12 +0300

    Created Player Character
    10.12.2024

---
+++ Source/GunSurvivors/TopdownCharacter.cpp
@@ -0,0 +1,215 @@
+
+
+#include "TopdownCharacter.h"
+
+#include "Kismet/KismetMathLibrary.h"
+
+ATopdownCharacter::ATopdownCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComp"));
+	SetRootComponent(CapsuleComp);
+
+	CharacterFlipbook = CreateDefaultSubobject<UPaperFlipbookComponent>(TEXT("CharacterFlipbook"));
+	CharacterFlipbook->SetupAttachment(RootComponent);
+
+	GunParent = CreateDefaultSubobject<USceneComponent>(TEXT("GunParent"));
+	GunParent->SetupAttachment(RootComponent);
+
+	GunSprite = CreateDefaultSubobject<UPaperSpriteComponent>(TEXT("GunSprite"));
+	GunSprite->SetupAttachment(GunParent);
+
+	BulletSpawnPosition = CreateDefaultSubobject<USceneComponent>(TEXT("BulletSpawnPosition"));
+	BulletSpawnPosition->SetupAttachment(GunSprite);
+
+}
+
+void ATopdownCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	APlayerController* PlayerController = Cast<APlayerController>(Controller);
+	if (PlayerController)
+	{
+		PlayerController->SetShowMouseCursor(true);
+
+		UEnhancedInputLocalPlayerSubsystem* Subsystem =
+			ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+
+		if (Subsystem)
+		{
+			Subsystem->AddMappingContext(InputMappingContext, 0);
+		}
+	}
+	
+}
+
+bool ATopdownCharacter::IsInMapBoundsHorizontal(float XPos)
+{
+	bool Result = true;
+
+	Result = (XPos > HorizontalLimits.X) && (XPos < HorizontalLimits.Y);
+
+	return Result;
+
+}
+
+bool ATopdownCharacter::IsInMapBoundsVertical(float ZPos)
+{
+
+	bool Result = true;
+
+	Result = (ZPos > VerticalLimits.X) && (ZPos < VerticalLimits.Y);
+
+	return Result;
+
+}
+
+
+
+
+void ATopdownCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	//Move Player
+	if(CanMove)
+	{
+		if (MovementDirection.Length() > 0.0f)
+		{
+			if (MovementDirection.Length() > 1.0f)
+			{
+				MovementDirection.Normalize();
+			}
+			FVector2D DistanceToMove = MovementDirection * MovementSpeed * DeltaTime;
+
+			FVector CurrentLocation = GetActorLocation();
+			FVector NewLocation = CurrentLocation + FVector(DistanceToMove.X, 0.0f, 0.0f);
+			if (!IsInMapBoundsHorizontal(NewLocation.X))
+			{
+				NewLocation -= FVector(DistanceToMove.X, 0.0f, 0.0f);
+			}
+
+			NewLocation += FVector(0.0f, 0.0f, DistanceToMove.Y);
+			if (!IsInMapBoundsVertical(NewLocation.Z))
+			{
+				NewLocation -= FVector(0.0f, 0.0f, DistanceToMove.Y);
+			}
+
+			SetActorLocation(NewLocation);
+		}
+	}
+
+	//Rotate Gun
+
+	APlayerController* PlayerController = Cast<APlayerController>(Controller);
+	if (PlayerController)
+	{
+		FVector MouseWorldLocation, MouseWorldDirection;
+		PlayerController->DeprojectMousePositionToWorld(MouseWorldLocation, MouseWorldDirection);
+
+
+		FVector CurrentLocation = GetActorLocation();
+		FVector Start = FVector(CurrentLocation.X, 0.0f, CurrentLocation.Z);
+		FVector Target = FVector(MouseWorldLocation.X, 0.0f, MouseWorldLocation.Z);
+		FRotator GunParentRotator = UKismetMathLibrary::FindLookAtRotation(Start, Target);
+
+		GunParent->SetRelativeRotation(GunParentRotator);
+	}
+
+
+}
+
+void ATopdownCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if (EnhancedInputComponent)
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ATopdownCharacter::MoveTriggered);
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Completed, this, &ATopdownCharacter::MoveCompleted);
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Canceled, this, &ATopdownCharacter::MoveCompleted);
+
+		EnhancedInputComponent->BindAction(ShootAction, ETriggerEvent::Started, this, &ATopdownCharacter::Shoot);
+		EnhancedInputComponent->BindAction(ShootAction, ETriggerEvent::Triggered, this, &ATopdownCharacter::Shoot);
+
+	}
+
+
+}
+
+void ATopdownCharacter::MoveTriggered(const FInputActionValue& Value)
+{
+	FVector2D MoveActionValue = Value.Get<FVector2D>();
+
+	if (CanMove)
+	{
+		MovementDirection = MoveActionValue;
+		CharacterFlipbook->SetFlipbook(RunFlipbook);
+
+		FVector FlipbookScale = CharacterFlipbook->GetComponentScale();
+		if (MovementDirection.X < 0.0f)
+		{
+			if(FlipbookScale.X > 0.0f)
+			{
+				CharacterFlipbook->SetWorldScale3D(FVector(-1.0f, 1.0f, 1.0f));
+			}
+		}
+		else if (MovementDirection.X > 0.0f)
+		{
+			if (FlipbookScale.X < 0.0f)
+			{
+				CharacterFlipbook->SetWorldScale3D(FVector(1.0f, 1.0f, 1.0f));
+			}
+		}
+
+	}
+}
+
+void ATopdownCharacter::MoveCompleted(const FInputActionValue& Value)
+{
+	MovementDirection = FVector2D(0.0f, 0.0f);
+	CharacterFlipbook->SetFlipbook(IdleFlipbook);
+
+}
+
+void ATopdownCharacter::Shoot(const FInputActionValue& Value)
+{
+	if (CanShoot)
+	{
+		CanShoot = false;
+
+		//Spawn Bullet Actor
+		ABullet *Bullet = GetWorld()->SpawnActor<ABullet>(BulletActorToSpawn, BulletSpawnPosition->GetComponentLocation(), 
+			FRotator(0.0f, 0.0f, 0.0f));
+		check(Bullet);
+
+		//Get Mouse World Location
+		APlayerController* PlayerController = Cast<APlayerController>(Controller);
+		check(PlayerController);
+		FVector MouseWorldLocation, MouseWorldDirection;
+		PlayerController->DeprojectMousePositionToWorld(MouseWorldLocation, MouseWorldDirection);
+
+		//Calculate Bullet Direction
+		FVector CurrentLocation = GetActorLocation();
+		FVector2D BulletDirection = FVector2D(MouseWorldLocation.X - CurrentLocation.X, MouseWorldLocation.Z - CurrentLocation.Z);
+		BulletDirection.Normalize();
+
+		//Launch The Bullet
+		float BulletSpeed = 300.0f;
+		Bullet->Launch(BulletDirection, BulletSpeed);
+
+
+
+		GetWorldTimerManager().SetTimer(ShootCooldownTimer, this, &ATopdownCharacter::OnShootCooldownTimerTimeout, 1.0f, false, ShootCooldowndDurationInSeconds);
+	}
+}
+
+void ATopdownCharacter::OnShootCooldownTimerTimeout()
+{
+	CanShoot = true;
+}
+
+
---
+++ Source/GunSurvivors/TopdownCharacter.h
@@ -0,0 +1,105 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SceneComponent.h"
+#include "PaperFlipbookComponent.h"
+#include "PaperSpriteComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Components/InputComponent.h"
+#include "InputActionValue.h"
+#include "GameFramework/Controller.h"
+#include "Engine/TimerHandle.h"
+#include "Bullet.h"
+
+
+
+#include "TopdownCharacter.generated.h"
+
+UCLASS()
+class GUNSURVIVORS_API ATopdownCharacter : public APawn
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UCapsuleComponent* CapsuleComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UPaperFlipbookComponent* CharacterFlipbook;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UInputMappingContext* InputMappingContext;	
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UPaperSpriteComponent* GunSprite;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	USceneComponent* GunParent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	USceneComponent* BulletSpawnPosition;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UInputAction* ShootAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UPaperFlipbook* IdleFlipbook;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UPaperFlipbook* RunFlipbook;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector2D HorizontalLimits;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector2D VerticalLimits;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<ABullet> BulletActorToSpawn;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MovementSpeed = 100.0f;
+
+	UPROPERTY(BlueprintReadWrite)
+	FVector2D MovementDirection;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	bool CanMove = true;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	bool CanShoot = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float ShootCooldowndDurationInSeconds = 0.3f;
+
+	FTimerHandle ShootCooldownTimer;
+
+
+	ATopdownCharacter();
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void MoveTriggered(const FInputActionValue& Value);
+	void MoveCompleted(const FInputActionValue& Value);
+
+	void Shoot(const FInputActionValue& Value);
+
+	bool IsInMapBoundsHorizontal(float XPos);
+	bool IsInMapBoundsVertical(float ZPos);
+
+	void OnShootCooldownTimerTimeout();
+
+};
